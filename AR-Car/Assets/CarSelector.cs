9792fb2065ed6f7f0e756df069a9d7ecc1df26fc
Jonathan <jruiz@deakin.edu.au>
2018-01-11 00:42:20 +1100

    Car model selection

---
+++ AR-Car/Assets/CarSelector.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelector : MonoBehaviour {
+    public GameObject a6;
+    public GameObject cube;
+    public GameObject sphere;
+
+    public int carSelected;
+
+	// Use this for initialization
+	void Start () {
+        a6.SetActive(true);
+        cube.SetActive(true);
+        sphere.SetActive(true);
+
+        carSelected = 1;
+   
+	}
+
+    public void loadA6()
+    {
+        a6.SetActive(true);
+        cube.SetActive(false);
+        sphere.SetActive(false);
+
+        carSelected = 1;
+    }
+
+    public void loadCube()
+    {
+        a6.SetActive(false);
+        cube.SetActive(true);
+        sphere.SetActive(false);
+
+        carSelected = 2;
+    }
+
+    public void loadSphere()
+    {
+        a6.SetActive(false);
+        cube.SetActive(false);
+        sphere.SetActive(true);
+
+        carSelected = 3;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
