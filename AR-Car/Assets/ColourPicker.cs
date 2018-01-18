34478134b22bc79d527dcf99ce59a7b99b4d3f48
Jonathan <jruiz@deakin.edu.au>
2018-01-18 12:35:36 +1100

    color and texture adjustments

---
+++ AR-Car/Assets/ColourPicker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourPicker : MonoBehaviour {
+
+    public Material[] BodyColourMat;
+    Material CurrMat;
+    Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = this.GetComponent<Renderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    //render blue colour
+    public void BlueColour()
+    {
+        renderer.material = BodyColourMat[0];
+        CurrMat = renderer.material;
+    }
+
+    //render red colour
+    public void RedColour()
+    {
+        renderer.material = BodyColourMat[1];
+        CurrMat = renderer.material;
+    }
+
+    //render green colour
+    public void GreenColour()
+    {
+        renderer.material = BodyColourMat[2];
+        CurrMat = renderer.material;
+    }
+
+    //render yellow colour
+    public void YellowColour()
+    {
+        renderer.material = BodyColourMat[3];
+        CurrMat = renderer.material;
+    }
+}
