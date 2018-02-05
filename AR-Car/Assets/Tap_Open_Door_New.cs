034760994d7821b4079c7a2697260094898efd8f
MicahNus <mnnuske@deakin.edu.au>
2018-02-05 13:43:26 +1100

    added car Rotate on 3D scene

---
+++ AR-Car/Assets/Tap_Open_Door_New.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; // Required when using Event data.
+
+public class Tap_Open_Door_New : MonoBehaviour, IPointerDownHandler, IPointerUpHandler // required interface when using the OnPointerDown method.
+
+{
+    bool pointerDown = false;
+    public Transform car;
+    int direction;
+
+    // Use this for initialization
+    void Start () {
+    
+        }
+
+// Update is called once per frame
+       void Update () {
+        
+        if (pointerDown == true)
+        {
+            foreach (Transform child in car)
+            {
+               
+                    child.transform.Rotate(0,direction, 0);
+                
+                
+            }
+        }
+
+        
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " Was Clicked.");
+        Debug.Log(eventData);
+        pointerDown = true;
+        if (this.gameObject.name == "btn_left_arrow")
+        {
+            direction = 1;
+        }
+        else if (this.gameObject.name == "btn_right_arrow")
+        {
+            direction = -1;
+        }
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pointerDown = false;
+    }
+}
