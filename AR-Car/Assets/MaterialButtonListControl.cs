519e55384175bc029362b42c53ceb533a5283976
MicahNus <mnnuske@deakin.edu.au>
2018-04-17 22:24:02 +1000

    Added dynamic texture menu

--- AR-Car/Assets/MaterialButtonListControl.cs
+++ AR-Car/Assets/MaterialButtonListControl.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class MaterialButtonListControl : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject buttonTemplate;
     public GameObject cars;
-
+    public Material[] materialList;
 
     // Use this for initialization
-    public void onClickAction(Material material)
+    void Start()
+    {
+
+
+        for (int i = 0; i < materialList.Length; ++i)
+        {
+            Debug.Log("got here mater");
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
+            //cars.transform.GetChild(i).GetComponent<CarControl>.carImage;
+            button.GetComponent<MaterialButtonMgr>().material = (materialList[i]);
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+
+        }
+    }
+
+
+
+
+
+        public void onClickAction(Material material)
     {
         for (int i = 0; i < cars.transform.childCount; i++)
         {
